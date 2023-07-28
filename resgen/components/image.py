16d89d13e1757122fbc9db7a3b292c485300ad55
aapman55 <zhi-li_liu@live.nl>
2023-07-28 22:59:25 +0200

    Compress profile picture by default

--- resgen/components/image.py
+++ resgen/components/image.py
@@ -7,9 +7,16 @@ from resgen.core.style import StyleRegistry
 
 class RoundedProfilePicture(Component):
     image_path: str = Field(..., description="Path to the image")
+    compress: bool = Field(
+        True, description="Whether to compress the image to save space"
+    )
 
     def add_pdf_content(self, doc: Document, style_registry: StyleRegistry):
         width = doc.w - doc.l_margin - doc.r_margin
+        original_image_filter = doc.image_filter
+        if self.compress:
+            doc.set_image_filter("DCTDecode")
+
         with doc.round_clip(
             x=doc.x,
             y=doc.y,
@@ -20,3 +27,4 @@ class RoundedProfilePicture(Component):
                 w=width,
                 keep_aspect_ratio=True,
             )
+        doc.set_image_filter(original_image_filter)
