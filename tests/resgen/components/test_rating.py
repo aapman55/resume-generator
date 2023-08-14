0d4d6e71891ede404fffaa90001c2041a07b7553
aapman55 <zhi-li_liu@live.nl>
2023-08-14 21:20:47 +0200

    Add tests for circle rating

--- tests/resgen/components/test_rating.py
+++ tests/resgen/components/test_rating.py
@@ -1,7 +1,15 @@
 import unittest
 
-from resgen.components.rating import draw_circle
+from pydantic import ValidationError
+
+from resgen.components.rating import (
+    draw_circle,
+    CircleRating,
+    CIRCLE_TO_FONT_SIZE_RATIO,
+)
 from resgen.core.document import Document
+from resgen.core.page_settings import SideBar
+from resgen.core.style import StyleRegistry
 
 
 class TestRating(unittest.TestCase):
@@ -17,3 +25,107 @@ class TestRating(unittest.TestCase):
         self.assertAlmostEqual(doc.x, 20)
         draw_circle(doc=doc, spacing=10, filled=False)
         self.assertAlmostEqual(doc.x, 30)
+
+
+class TestCircleRating(unittest.TestCase):
+    def setUp(self) -> None:
+        self.style_registry = StyleRegistry(
+            styles=[
+                {
+                    "id": "default",
+                    "family": "helvetica",
+                },
+            ]
+        )
+
+    def test_init(self):
+        CircleRating(rating=3, rating_text="dummy_rating", rating_text_style="default")
+
+    def test_init_invalid_rating(self):
+        with self.assertRaises(ValueError):
+            """
+            rating total smaller than rating
+            """
+            CircleRating(
+                rating_total=2,
+                rating=3,
+                rating_text="dummy_rating",
+                rating_text_style="default",
+            )
+
+        with self.assertRaises(ValidationError):
+            """
+            Rating smaller than 0
+            """
+            CircleRating(
+                rating=-1, rating_text="dummy_rating", rating_text_style="default"
+            )
+
+        with self.assertRaises(ValidationError):
+            """
+            rating_total equal to 0
+            """
+            CircleRating(
+                rating_total=0,
+                rating=0,
+                rating_text="dummy_rating",
+                rating_text_style="default",
+            )
+
+    def test_add_pdf_content(self) -> None:
+        doc = Document()
+        doc.add_page()
+        rating = CircleRating(
+            rating=3, rating_text="dummy_rating", rating_text_style="default"
+        )
+
+        self.assertAlmostEqual(doc.y, 10)
+
+        rating.add_pdf_content(doc=doc, style_registry=self.style_registry)
+
+        adjust_y_for_circle_size = (1 - CIRCLE_TO_FONT_SIZE_RATIO) / 2 * doc.font_size
+        self.assertAlmostEqual(
+            doc.y,
+            # Starting point
+            10
+            # dummy_rating does not fit on 1 line, so extra line
+            + doc.font_size
+            # Reposition due to circle size
+            + adjust_y_for_circle_size
+            # This one is after the circles
+            + doc.font_size,
+        )
+
+    def test_add_pdf_content_multi_line_circles(self) -> None:
+        width = 60
+        sidebar = SideBar(
+            width=width,
+            align_left=True,
+        )
+        doc = Document(sidebar=sidebar)
+        doc.add_page()
+        rating = CircleRating(
+            rating_total=15,
+            rating=7,
+            rating_text="dummy_rating",
+            rating_text_style="default",
+        )
+        doc.switch_to_sidebar()
+        self.assertAlmostEqual(doc.y, 10)
+
+        rating.add_pdf_content(doc=doc, style_registry=self.style_registry)
+
+        adjust_y_for_circle_size = (1 - CIRCLE_TO_FONT_SIZE_RATIO) / 2 * doc.font_size
+        self.assertAlmostEqual(
+            doc.y,
+            # Starting point
+            10
+            # dummy_rating does not fit on 1 line, so extra line
+            + doc.font_size
+            # Reposition due to circle size
+            + adjust_y_for_circle_size
+            # circles just fit on 2 lines so extra font_size
+            + doc.font_size
+            # This one is after the circles
+            + doc.font_size,
+        )
